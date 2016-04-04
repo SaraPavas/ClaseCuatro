4a686ac30931cc95f807cea52af8f5838c21d84e
SaraPavas <sara.pavas@udea.edu.co>
2016-04-04 00:24:07 -0500

    version 2

--- notasapp/src/main/java/com/example/sarapavas/notasapp/MainActivity.java
+++ notasapp/src/main/java/com/example/sarapavas/notasapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sarapavas.notasapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,10 +15,12 @@ import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    MainActivity objeto;
     EditText ePrac, eExpo, eProy;
-    TextView tNotaFinal;
+    TextView tNotaFinal,tpProy,tpPrac,tpExpo;
     Button calcular;
+    Button borrar;
+    double pExpo=15, pProy=35, pPrac=50;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,25 +32,73 @@ public class MainActivity extends AppCompatActivity {
         eExpo = (EditText)findViewById(R.id.expo);
         calcular = (Button)findViewById(R.id.calc);
         tNotaFinal = (TextView)findViewById(R.id.NotaFinal);
+        tpProy= (TextView)findViewById(R.id.tpProy);
+        tpPrac= (TextView)findViewById(R.id.tpPrac);
+        tpExpo= (TextView)findViewById(R.id.tpExpo);
+        borrar = (Button)findViewById(R.id.borrar);
         calcular.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                double exp, proy, notaFinal, prac;
-                exp = Double.parseDouble(eExpo.getText().toString());
-                proy =  Double.parseDouble(eProy.getText().toString());
-                prac =  Double.parseDouble(ePrac.getText().toString());
 
-                notaFinal = 0.15*exp + 0.35*proy+0.5*prac;
-
-                tNotaFinal.setText(Double.toString(notaFinal));
-
-
+                boolean control;
+                control = espaciosEnBlanco();
+                if(control) {
+                    if (validarNotas()) {
+                        calcularNotas();
+                    }
+                }
+            }
+        });
+        borrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                tNotaFinal.setText("");
+                eExpo.getText().clear();
+                eProy.getText().clear();
+                ePrac.getText().clear();
             }
         });
 
     }
+    public boolean espaciosEnBlanco(){
+        String sExp, sProy, sPrac;
+        sExp=eExpo.getText().toString();
+        sProy = eProy.getText().toString();
+        sPrac = ePrac.getText().toString();
+        if(TextUtils.isEmpty(sExp)||TextUtils.isEmpty(sProy)||TextUtils.isEmpty(sPrac)) {
+            Toast.makeText(this,  getResources().getString(R.string.EspaciosBlanco), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    public boolean validarNotas() {
+        double exp, proy, notaFinal, prac;
+        exp = Double.parseDouble(eExpo.getText().toString());
+        proy = Double.parseDouble(eProy.getText().toString());
+        prac = Double.parseDouble(ePrac.getText().toString());
+        if (exp < 0.0 || exp > 5.0) {
+            Toast.makeText(this,  getResources().getString(R.string.notaexpo), Toast.LENGTH_SHORT).show();
+            return false;
+        }if( proy <0.0 || proy > 5.0) {
+            Toast.makeText(this,  getResources().getString(R.string.notaproy), Toast.LENGTH_SHORT).show();
+            return false;
+        }if (prac < 0.0 || prac > 5.0) {
+            Toast.makeText(this,  getResources().getString(R.string.notaprac), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    public void calcularNotas() {
+        double exp, proy, notaFinal, prac;
+        exp = Double.parseDouble(eExpo.getText().toString());
+        proy = Double.parseDouble(eProy.getText().toString());
+        prac = Double.parseDouble(ePrac.getText().toString());
+
+        notaFinal = (pExpo * exp + pProy * proy + pPrac * prac)/100;
+        notaFinal = Math.rint(notaFinal*10)/10;
+        tNotaFinal.setText(Double.toString(notaFinal));
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { //inflarnos el menu
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
@@ -56,14 +107,30 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.menu_configurar){
-            Toast.makeText(this, "Ha presionado opcion configurar",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(this, settings.class);
-            i.putExtra("pProy",35);
-            i.putExtra("pExp",15);
-            i.putExtra("pPrac",50);
-            startActivity(i);
+            Toast.makeText(this, getResources().getString(R.string.bConfigurar),Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, settings.class); //iniciar activitades this = contexto
+            i.putExtra("pProy",pProy);//mandar datos no me interesa el tipo
+            i.putExtra("pExp",pExpo);
+            i.putExtra("pPra",pPrac);
+            startActivityForResult(i, 1234);//intent y codigo para identificar la respuesta
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode ==1234 && resultCode==RESULT_OK){
+
+            pProy = data.getExtras().getDouble("prPro");
+            pPrac = data.getExtras().getDouble("prPra");
+            pExpo = data.getExtras().getDouble("prExpo");
+            Toast.makeText(this, "Proyecto = "+Double.toString(pProy)+" Exposicion = "
+                    + Double.toString(pExpo)+ " Practicas = "+Double.toString(pPrac),
+                    Toast.LENGTH_SHORT).show();
+            tpProy.setText("(" + Double.toString(pProy) + "%) ");
+            tpPrac.setText("("+Double.toString(pPrac)+"%) ");
+            tpExpo.setText("("+Double.toString(pExpo)+"%) ");
+        }
     }
 }
--- notasapp/src/main/java/com/example/sarapavas/notasapp/settings.java
+++ notasapp/src/main/java/com/example/sarapavas/notasapp/settings.java
@@ -2,13 +2,21 @@ package com.example.sarapavas.notasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class settings extends AppCompatActivity {
 
     EditText ePra, eExp, ePro;
-    Button bSave;
+    Button bSave,bLimpiar;
+    TextView tpPrac, tpProy, tpExpo;
+    boolean limpio=false;
+    String sExp, sPro,sPra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +26,80 @@ public class settings extends AppCompatActivity {
         ePra = (EditText)findViewById(R.id.Pprac);
         ePro= (EditText)findViewById(R.id.Pproy);
         eExp = (EditText)findViewById(R.id.Pexpo);
+        tpPrac =(TextView)findViewById(R.id.tpPrac);
+        tpProy =(TextView)findViewById(R.id.tpProy);
+        tpExpo =(TextView)findViewById(R.id.tpExpo);
         bSave = (Button)findViewById(R.id.guardar);
+        bLimpiar = (Button)findViewById(R.id.borrar);
 
-        Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras(); //captura los extras
 
-        eExp.setText(String.valueOf(extras.getInt("pExp")));
-        ePra.setText(String.valueOf(extras.getInt("pPra")));
-        ePro.setText(String.valueOf(extras.getInt("pProy")));
+        eExp.setText(String.valueOf(extras.getDouble("pExp"))); //los pone en los editext
+        ePra.setText(String.valueOf(extras.getDouble("pPra")));
+        ePro.setText(String.valueOf(extras.getDouble("pProy")));
+        sExp = eExp.getText().toString();
+        sPra = ePra.getText().toString();
+        sPro = ePro.getText().toString();
+        bSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //responde a la actividad principal
+                Intent i = new Intent();
+                //boolean control=false;
+                int re_espBlanco;
+                re_espBlanco = espaciosEnBlanco();
+                if(re_espBlanco == 1) {
+                    if (verificarPorcentajes()) {
+                        i.putExtra("prPro", Double.parseDouble(ePro.getText().toString()));
+                        i.putExtra("prPra", Double.parseDouble(ePra.getText().toString()));
+                        i.putExtra("prExpo", Double.parseDouble(eExp.getText().toString()));
+                        setResult(RESULT_OK, i);
+                        finish();
+                    }
+                }else  if(re_espBlanco == 2) {
+                    finish();
+                }
 
+            }
+        });
+        bLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tpPrac.setText("("+sPra+"%) ");
+                tpProy.setText("("+sPro+"%) ");
+                tpExpo.setText("("+sExp+"%) ");
+                eExp.getText().clear();
+                ePro.getText().clear();
+                ePra.getText().clear();
+                limpio = true;
+            }
+        });
+    }
+    int espaciosEnBlanco(){
+        String sExpo, sProy, sPrac;
+        sExpo=eExp.getText().toString();
+        sProy = ePro.getText().toString();
+        sPrac = ePra.getText().toString();
+        //Bundle extras = getIntent().getExtras();
+        if(TextUtils.isEmpty(sExpo)&&TextUtils.isEmpty(sProy)&&TextUtils.isEmpty(sPrac)) {
+            Toast.makeText(this, getResources().getString(R.string.modificar), Toast.LENGTH_SHORT).show();
+            return 2;
+        }
+        if(sExpo.equals(sExp)  && sProy.equals(sPro)  && sPrac.equals(sPra)){
+            Toast.makeText(this, getResources().getString(R.string.modificar), Toast.LENGTH_SHORT).show();
+            return 2;
+        }
+        if(TextUtils.isEmpty(sExpo)||TextUtils.isEmpty(sProy)||TextUtils.isEmpty(sPrac)) {
+            Toast.makeText(this, getResources().getString(R.string.EspaciosBlanco), Toast.LENGTH_SHORT).show();
+            return 0;
+        }
+        return 1;
+    }
+    public boolean verificarPorcentajes(){
+        if(Double.parseDouble(ePro.getText().toString()) + Double.parseDouble(ePra.getText().toString())
+                + Double.parseDouble(eExp.getText().toString()) != 100) {
+            Toast.makeText(this, getResources().getString(R.string.suma), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
